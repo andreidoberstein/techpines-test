dd580d0677512ca4e16d01e7e903d79a77e4d1ea
Andrei <andrei.vupt@gmail.com>
2025-09-07 17:15:14 -0300

    feat: create config deploy

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -2,6 +2,9 @@
 # Iniciar PHP-FPM em segundo plano
 php-fpm -D
 
+# Ajustar permissões do script de deploy
+chmod +x /var/www/html/scripts/00-laravel-deploy.sh
+
 # Executar script de deploy
 echo "Executing deploy script..."
 /var/www/html/scripts/00-laravel-deploy.sh
