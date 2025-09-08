9d2863c8101dee0b57576161c5f3d6af4b5c3510
Andrei <andrei.vupt@gmail.com>
2025-09-08 14:38:23 -0300

    feat: config cors deploy

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,7 +8,8 @@ return [
     'http://127.0.0.1:5173',
     'http://localhost:8081',
     'http://localhost:8080',
-    'https://techpines-test-8pwjok9ur-andreivupts-projects.vercel.app'
+    'https://techpines-test-8pwjok9ur-andreivupts-projects.vercel.app',
+    'https://techpines-test.vercel.app'
   ],
   'allowed_headers' => ['*'],
   'exposed_headers' => [],
